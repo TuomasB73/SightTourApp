df414b883b4777ebc42c274bd2b4fd881289440a
TuomasB73 <tuomasbergholm@gmail.com>
2022-11-17 15:53:31 +0200

    Custom menu bar

--- app/src/main/java/fi/urbanmappers/sighttour/MainActivity.kt
+++ app/src/main/java/fi/urbanmappers/sighttour/MainActivity.kt
@@ -3,6 +3,8 @@ package fi.urbanmappers.sighttour
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.LinearLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.commit
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -19,7 +21,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        //bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        val bottomTabNavBar = findViewById<FrameLayout>(R.id.bottomTabNavBar)
         fragmentContainer = findViewById(R.id.fragmentContainer)
 
         val toursFragment = ToursFragment()
@@ -29,7 +32,19 @@ class MainActivity : AppCompatActivity() {
 
         setCurrentFragment(toursFragment)
 
-        bottomNavigationView.setOnItemSelectedListener {
+        bottomTabNavBar.findViewById<ImageButton>(R.id.btn_nature_id).setOnClickListener {
+            setCurrentFragment(toursFragment)
+        }
+        bottomTabNavBar.findViewById<ImageButton>(R.id.menu_btn_map).setOnClickListener {
+            setCurrentFragment(mapFragment)
+        }
+        bottomTabNavBar.findViewById<ImageButton>(R.id.menu_btn_places).setOnClickListener {
+            setCurrentFragment(placesFragment)
+        }
+        bottomTabNavBar.findViewById<ImageButton>(R.id.menu_btn_activities).setOnClickListener {
+            setCurrentFragment(activitiesAndEventsFragment)
+        }
+        /*bottomNavigationView.setOnItemSelectedListener {
             when (it.itemId) {
                 R.id.tours -> setCurrentFragment(toursFragment)
                 R.id.map -> setCurrentFragment(mapFragment)
@@ -37,7 +52,7 @@ class MainActivity : AppCompatActivity() {
                 R.id.activitiesAndEvents -> setCurrentFragment(activitiesAndEventsFragment)
             }
             true
-        }
+        }*/
     }
 
     private fun setCurrentFragment(fragment: Fragment) {
