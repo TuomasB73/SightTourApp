a79e9050e62ebf23552f266a1bdeacda8b8effd7
TuomasB73 <tuomasbergholm@gmail.com>
2022-11-24 15:05:33 +0200

    Tours categories icons visible according to the data

--- app/src/main/java/fi/urbanmappers/sighttour/adapters/ToursListRecyclerViewAdapter.kt
+++ app/src/main/java/fi/urbanmappers/sighttour/adapters/ToursListRecyclerViewAdapter.kt
@@ -12,6 +12,8 @@ import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import fi.urbanmappers.sighttour.R
 import fi.urbanmappers.sighttour.datamodels.Tour
+import fi.urbanmappers.sighttour.utils.ToursCategory
+import fi.urbanmappers.sighttour.utils.ToursMobilityMethod
 
 class ToursListRecyclerViewAdapter(
     private val tourItems: List<Tour>,
@@ -52,6 +54,53 @@ class ToursListRecyclerViewAdapter(
         holder.distanceDurationTextView.text = "$totalDistance km, $totalDuration min"
 
         Glide.with(context).load(tourItems[position].imageUrl).centerCrop().into(holder.imageView)
+
+        val categories = tourItems[position].categories
+
+        if (categories.any { it == ToursCategory.Nature }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.natureCategoryImageView).visibility = View.VISIBLE
+        }
+        if (categories.any { it == ToursCategory.Shopping }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.shoppingCategoryImageView).visibility = View.VISIBLE
+        }
+        if (categories.any { it == ToursCategory.City }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.cityCategoryImageView).visibility = View.VISIBLE
+        }
+        if (categories.any { it == ToursCategory.Culture }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.cultureCategoryImageView).visibility = View.VISIBLE
+        }
+        if (categories.any { it == ToursCategory.Sports }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.sportsCategoryImageView).visibility = View.VISIBLE
+        }
+        if (categories.any { it == ToursCategory.Sightseeing }) {
+            holder.tourCategoryIconsContainer.findViewById<ImageView>(R.id.sightseeingCategoryImageView).visibility = View.VISIBLE
+        }
+
+        val mobilityMethods = tourItems[position].tripStages.map {
+            it.mobilityMethod
+        }
+
+        if (mobilityMethods.any { it == ToursMobilityMethod.Walking }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.walkingMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.Bicycling }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.bicyclingMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.EScooter }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.escooterMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.Bus }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.busMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.Tram }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.tramMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.Metro }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.metroMobilityMethodImageView).visibility = View.VISIBLE
+        }
+        if (mobilityMethods.any { it == ToursMobilityMethod.Ferry }) {
+            holder.mobilityMethodIconsContainer.findViewById<ImageView>(R.id.ferryMobilityMethodImageView).visibility = View.VISIBLE
+        }
 
         holder.itemView.setOnClickListener {
             tourItemClickListener.onTourItemClick(tourItems[position].id)
